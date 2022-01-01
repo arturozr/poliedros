28bc1b879f1e1d96ee9e8618d47685f3282db030
arturozr <azrcuentanosegura@gmail.com>
2021-12-31 18:17:43 -0600

    Agregada la prueba de solo numeros #2

--- src/app/pruebas/pruebas.spec.ts
+++ src/app/pruebas/pruebas.spec.ts
@@ -12,14 +12,36 @@ describe('pruebas', () => {
   });
 
 
-it('should retornar "Si es un poliedro" cuando los valores introducidos den como resultado 2', () => {
+it('Deberia retornar "Si es un poliedro" cuando los valores introducidos den como resultado 2', () => {
   expect(component.verificar(8,12,6)).toContain("Si es un poliedro");
   
 });
 
-it('should retornar "No es un poliedro" cuando los valores introducidos den como resultado un numero diferente de 2', () => {
+it('Deberia retornar "No es un poliedro" cuando los valores introducidos den como resultado un numero diferente de 2', () => {
   expect(component.verificar(8,12,4)).toContain("No es un poliedro");
   
 });
+
+
+it('Deberia retornar "Debe introducir solo numeros" cuando los valores introducidos no sean de tipo number', () => {
+  expect(component.verificar("V","E","F")).toContain("Debe introducir solo numeros");
+});
+
+it('Deberia retornar "Debe introducir solo numeros" cuando los valores introducidos no sean de tipo number', () => {
+  expect(component.verificar(8,"E","F")).toContain("Debe introducir solo numeros");
+});
+
+it('Deberia retornar "Debe introducir solo numeros" cuando los valores introducidos no sean de tipo number', () => {
+  expect(component.verificar(8,12,"F")).toContain("Debe introducir solo numeros");
+});
+
+it('Deberia retornar "Debe introducir solo numeros" cuando los valores introducidos no sean de tipo number', () => {
+  expect(component.verificar("V",12,6)).toContain("Debe introducir solo numeros");
+});
+
+it('Deberia retornar "Debe introducir solo numeros" cuando los valores introducidos no sean de tipo number', () => {
+  expect(component.verificar(8,"E",6)).toContain("Debe introducir solo numeros");
+});
+
 
 });
